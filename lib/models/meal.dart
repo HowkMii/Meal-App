197710d8eda916acf0626763f871ee2b22cb4dd4
unknown <abdelhakim@univ-tiaret.dz>
2021-07-17 14:20:05 +0100

    meal class

---
+++ lib/models/meal.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class Meal{
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian; 
+  
+}
