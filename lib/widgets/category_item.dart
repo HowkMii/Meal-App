d0a4b8038037ee8386101e1eca644a733a890ee5
unknown <abdelhakim@univ-tiaret.dz>
2021-07-18 14:38:32 +0100

    CategoryItem

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+  
+  CategoryItem(
+    
+    this.id,
+    this.title,
+    this.color ,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
