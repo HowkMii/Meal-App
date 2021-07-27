b373d6ce61697421b832ad1b8cbd65e455f6e93e
howkmii <abdelhakim@univ-tiaret.dz>
2021-07-27 15:14:21 +0100

    body meal

--- lib/screens/meal__detail_screen.dart
+++ lib/screens/meal__detail_screen.dart
@@ -10,13 +10,13 @@ class MealDetailScreen extends StatelessWidget {
     final mealId = ModalRoute.of(context).settings.arguments as String;
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id ==mealId);
     return Scaffold(
-      appBar: AppBar(title: Text(mealId)),
+      appBar: AppBar(title: Text(selectedMeal.title)),
       body: Column(
         children: [
           Container(
             width:double.infinity,
             height: 300,
-            child: Image.network(selectedMeal.imageUrl),
+            child: Image.network(selectedMeal.imageUrl, fit: BoxFit.cover),
           
           ),
         ],
