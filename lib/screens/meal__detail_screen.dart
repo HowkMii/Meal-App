b532406c1b264e0b289d349a4fdb0e54cbe55110
howkmii <abdelhakim@univ-tiaret.dz>
2021-07-28 14:13:25 +0100

    part 8

--- lib/screens/meal__detail_screen.dart
+++ lib/screens/meal__detail_screen.dart
@@ -24,11 +24,17 @@ class MealDetailScreen extends StatelessWidget {
             child: Text("ingradient",style: Theme.of(context).textTheme.title,),
           ),
           Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
             height: 200,
             width: 300,
             child: ListView.builder(
               itemBuilder: (ctx, index)=>Card(
                 color:Theme.of(context).accentColor,
+                child: Text(selectedMeal.ingredients[index]),
               ),
               itemCount: selectedMeal.ingredients.length,
 
