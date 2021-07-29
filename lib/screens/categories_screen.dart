5c12e9a87dbac2329716fd3ca34825fc74485668
howkmii <abdelhakim@univ-tiaret.dz>
2021-07-29 19:40:00 +0100

    Finish

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:meal/dummy_data.dart';
-
 import 'package:meal/widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Meal"),),
+      
       body: GridView(
         padding: EdgeInsets.all(25),
         children: DUMMY_CATEGORIES.map((catData) => 
