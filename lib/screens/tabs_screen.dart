70d8d1d59711c478a72933dd52bbd7020ad9cc54
howkmii <abdelhakim@univ-tiaret.dz>
2021-07-30 14:15:20 +0100

    drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meal/screens/categories_screen.dart';
 import 'package:meal/screens/favotites_screen.dart';
+import 'package:meal/widgets/main_drawer.dart';
 class TabsScreen  extends StatefulWidget {
   @override
   _TabsScreenState createState() => _TabsScreenState();
@@ -42,6 +43,7 @@ class _TabsScreenState extends State<TabsScreen> {
             title: Text("Favorites"),),
         ],
       ),
+      drawer: MainDrawer(),
     );
   }
 
