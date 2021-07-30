ad19621128876f8e768176a4f566362b6a1800a5
howkmii <abdelhakim@univ-tiaret.dz>
2021-07-30 14:31:52 +0100

    drawer creating

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -6,11 +6,23 @@ class MainDrawer extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            height: 120,
+            height: 130,
             width: double.infinity,
-            padding: EdgeInsets.all(20),
+            padding: EdgeInsets.all(30),
             alignment: Alignment.centerLeft,
-            child: Text("cooking up"),
+            color: Theme.of(context).primaryColor,
+
+            child: Text('Cooking up!',style: TextStyle(
+              fontSize: 26,fontWeight: FontWeight.w900,color: Theme.of(context).accentColor
+            ),),
+          ),
+          SizedBox(
+            height: 20,
+
+          ),
+          ListTile(
+            leading: Icon(Icons.restaurant, size: 25,),
+            title: Text("Meal"),
           )
         ],
         
