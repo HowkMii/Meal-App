dda9f434a85321b69cbfee5d461058562647ce9c
howkmii <abdelhakim@univ-tiaret.dz>
2021-07-30 14:23:03 +0100

    drawer child

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -3,6 +3,17 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            child: Text("cooking up"),
+          )
+        ],
+        
+      ),
       
     );
   }
