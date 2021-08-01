8ffc19499f925bce4f3db18e82f16a01152839ac
howkmii <abdelhakim@univ-tiaret.dz>
2021-08-01 16:23:47 +0100

    add

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:meal/widgets/main_drawer.dart';
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName ='/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+   bool _glutenFree = false;
+   bool _lactoseFree = false;
+   bool _vegan = false;
+   bool _vegetarian = false; 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
